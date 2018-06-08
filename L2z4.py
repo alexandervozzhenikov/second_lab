64be9724b0157bee2781ecdcaa54f29aec8cd169
Alexander <39276672+alexandervozzhenikov@users.noreply.github.com>
2018-06-08 10:12:47 +0500

    Update L2z4.py

--- L2z4.py
+++ L2z4.py
@@ -1,11 +1,7 @@
-import math
-a = int(input("a="))
-b = int(input("b="))
-c = int(input("c="))
-if(c>a) and (c>b) and (c=math.sqrt(sqr(a)+sqr(b)))
-    print("Является прямоугольным")
-if (a>c) and (b>c) and (b=math.sqrt(sqr(c)+sqr(b)))
-    print("Является прямоугольным")
-if (b>a) and (b>c) and (b=math.sqrt(sqr(a)+sqr(c)))
+a = float(input("a="))
+b = float(input("b="))
+c = float(input("c="))
+if ((a*a == b* b +c*c) or (b*b == a*a+c*c) or (c*c == a*a + b*b)):
+    print('Является прямоугольным')
 else:
     print("Не является прямоугольным")
