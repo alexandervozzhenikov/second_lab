b6617f8485a7858abc721ed382667d30e3832bdf
Alexander <39276672+alexandervozzhenikov@users.noreply.github.com>
2018-06-08 10:11:25 +0500

    Update L2z3.py

--- L2z3.py
+++ L2z3.py
@@ -1,7 +1,6 @@
-import math
-x = float(input("x="))
-y = float(input("y="))
-if (math.sqrt(x * x + y * y) <= 1 and ((x + 2) ** 2 + y * y) <= 2):
+x = int(input("x="))
+y = int(input("y="))
+if (y > x and y > -1 and x < 1):
     print("точка входит в закрашенную область")
 else:
     print("точка не входит в закрашенную область")
